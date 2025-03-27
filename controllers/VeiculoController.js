33f7585436d95011e6fe416c054ad17202a4a06f
Leo Silva <leonardobeneval@gmail.com>
2025-03-26 22:09:24 -0400

    Post

--- controllers/VeiculoController.js
+++ controllers/VeiculoController.js
@@ -4,7 +4,9 @@ class VeiculoController {
     static formCadastro(req, res){
         res.sendFile(path.join(__dirname, "../", "views", "formVeiculo.html"))
     }
-    static cadastrar(req, res){}
+    static cadastrar(req, res){
+        res.send(JSON.stringify(req.body));
+    }
 
     static buscarTodos(req, res){}
 }
