6a3794be2e7b14fbcd8dda3ddc48882856ff6e4c
Eoleonardo <leonardobeneval@gmail.com>
2025-04-01 10:05:51 -0400

    Prisma_BD

--- controllers/VeiculoController.js
+++ controllers/VeiculoController.js
@@ -1,14 +1,31 @@
 const path = require("path");
 
+const {PrismaClient} = require("@prisma/client");
+const client = new PrismaClient(); 
+
 class VeiculoController {
     static formCadastro(req, res){
         res.sendFile(path.join(__dirname, "../", "views", "formVeiculo.html"))
     }
-    static cadastrar(req, res){
-        res.send(JSON.stringify(req.body));
+    static async cadastrar(req, res){
+
+        const {modelo, placa, ano, cor} = req.body;
+
+        const veiculo = await client.veiculo.create({data:{
+          modelo,
+          placa,
+          ano: parseInt(ano),
+          cor
+        }});
+
+        res.send({veiculo});
     }
 
     static buscarTodos(req, res){}
 }
 
 module.exports = VeiculoController
+
+
+// npx prisma migrate dev
+// npx prisma generate
